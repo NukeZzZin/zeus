8d13644009c7ec59d35ecbd571bf59814f95f04e
João Vitor Vieira Contin <nukezzzin@gmail.com>
2026-03-25 19:06:02 -0300

    [chore] improvement codde style

--- src/components/Protected.tsx
+++ src/components/Protected.tsx
@@ -1,8 +1,6 @@
 import { Navigate } from "react-router";
 import { useIsLoggedIn } from "@stores/auth_store";
 
-const Protected = ({ children }: { children: React.ReactNode }) => {
-  return useIsLoggedIn() ? children : <Navigate to="/login" replace/>;
-};
+const Protected = ({ children }: { children: React.ReactNode }) => useIsLoggedIn() ? children : <Navigate to="/login" replace/>;
 
 export default Protected;
